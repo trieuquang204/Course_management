e6eef4da62c4e18491d6b752585f31148c8cf6d7
quang.trieu <quang.trieu@lifesup.com.vn>
2023-01-31 17:14:31 +0700

    Add bootstrap and template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,56 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { RouterModule, Routes } from '@angular/router';
+
 import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { CoursesComponent } from './components/courses/courses.component';
+import { CourseListComponent } from './components/course-list/course-list.component';
+import { CourseAddComponent } from './components/course-add/course-add.component';
+import { CourseEditComponent } from './components/course-edit/course-edit.component';
+
+const appRoutes : Routes = [
+ {
+  path: '',
+  component: HomeComponent,
+ },
+ {
+  path: 'courses',
+  component: CoursesComponent,
+ },
+ {
+  path: 'courses',
+  component: CoursesComponent,
+  children: [
+    {
+      path: 'list',
+      component: CourseListComponent
+    },
+    {
+      path: 'add',
+      component: CourseAddComponent
+    },
+    {
+      path: ':id',
+      component: CourseEditComponent
+    },
+  ]
+ },
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    CoursesComponent,
+    CourseListComponent,
+    CourseAddComponent,
+    CourseEditComponent,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes),
   ],
   providers: [],
   bootstrap: [AppComponent]
