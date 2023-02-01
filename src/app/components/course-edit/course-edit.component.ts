f84b8b32e88b7b9d89f1993e1d1aeca424b3835e
quang.trieu <quang.trieu@lifesup.com.vn>
2023-02-01 13:56:36 +0700

    Hoan thanh chuc nang sua update

--- src/app/components/course-edit/course-edit.component.ts
+++ src/app/components/course-edit/course-edit.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+
+import { CourseService } from 'src/app/services/course.service';
+import { Subscription, Observable } from 'rxjs';
+import { Router, ActivatedRoute } from '@angular/router';
+
+
+import { Course } from 'src/app/models/course.model';
 
 @Component({
   selector: 'app-course-edit',
   templateUrl: './course-edit.component.html',
   styleUrls: ['./course-edit.component.css']
 })
-export class CourseEditComponent {
+export class CourseEditComponent implements OnInit {
 
+  // public courses : any = [];
+  // public name : string = '';
+  // public description : string = '';
+  // public fee : any = null;
+  public courseEdit? : any;
+
+  public subscription? : Subscription;
+
+  constructor(
+    public courseService: CourseService,
+    public routerService: Router,
+    public activatedRoute: ActivatedRoute,
+  ) {}
+
+  ngOnInit() {
+    // this.subscription = this.courseService.getAllCourses().subscribe((data : any) => {
+    //   this.courses = data;
+    // })
+
+    this.loadData();
+  }
+
+  loadData() {
+    this.activatedRoute.params.subscribe(data => {
+      let id = data['id'];
+      this.courseService.getCourseEdit(id).subscribe(course => {
+        // console.log('test', course)
+        this.courseEdit = course
+      })
+    })
+  }
+
+  onEditSave() {
+    this.courseService.updateCourse(this.courseEdit).subscribe((data) => {
+      this.routerService.navigate(['/courses/list'])
+    })
+  }
 }
--- src/app/services/course.service.ts
+++ src/app/services/course.service.ts
@@ -17,11 +17,19 @@ export class CourseService {
     return this.http.get(this.API)
   }
 
+  getCourseEdit(id: any) : Observable<Course> {
+    return this.http.get(`${this.API}/${id}`)
+  }
+
   addCourse(course : any) : Observable<Course> {
     return this.http.post(this.API, course)
   }
 
   deleteCourse(id: any) : Observable<Course> {
     return this.http.delete(`${this.API}/${id}`)
+  }
+
+  updateCourse(course : any) : Observable<Course> {
+    return this.http.put(`${this.API}/${course.id}`, course)
   }
 }
